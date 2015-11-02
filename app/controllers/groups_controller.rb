e5127525763f2bb04b4cabf3f65e663a13b89209
wesleychu <jrubyuncle@gmail.com>
2015-11-02 14:57:55 +0800

    complete groups#new and groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,4 +3,26 @@ class GroupsController < ApplicationController
     #flash[:notice] = "this-is-notice"
     @groups = Group.all
   end
+
+  def show
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+
+    @group = Group.new group_params
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
