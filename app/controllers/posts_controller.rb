ac05038f688d11d8e104bfdd39c61e1098121deb
wesleychu <jrubyuncle@gmail.com>
2015-11-02 16:30:50 +0800

    posts#new and posts#create ok

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,18 @@
 class PostsController < ApplicationController
   def new
+    @group = Group.find params[:group_id]
+    @post = @group.posts.new
   end
 
   def create
+    @group = Group.find params[:group_id]
+    @post = @group.posts.new post_params
+
+    if @post.save
+      redirect_to group_path(@group), notice: "post created ok"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -14,4 +24,8 @@ class PostsController < ApplicationController
   def destroy
   end
 
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
