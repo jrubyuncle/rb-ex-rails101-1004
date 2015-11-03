d8ddf4a827234a0193719a1eb4fc439bb2b69b98
wesleychu <jrubyuncle@gmail.com>
2015-11-03 12:11:28 +0800

    add edit_user_registration_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) do |u|
     	u.permit(:name, :email, :password, :password_confirmation)
     end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+    	u.permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
   end
-
 end
