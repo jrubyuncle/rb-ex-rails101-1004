63c6e6c539b0f6b6143788603d00b999ac1b3172
wesleychu <jrubyuncle@gmail.com>
2015-11-03 19:14:02 +0800

    use counter_cache

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  belongs_to :group
+  belongs_to :group, counter_cache: :posts_count
   belongs_to :author, foreign_key: :user_id, class_name: 'User'
 
   validates :content, presence: true
