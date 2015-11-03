0c489a52c03eb688f824bd61ba0e230368c728d2
wesleychu <jrubyuncle@gmail.com>
2015-11-03 18:02:33 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103063326) do
+ActiveRecord::Schema.define(version: 20151103100117) do
 
   create_table "group_users", force: :cascade do |t|
     t.integer  "group_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20151103063326) do
     t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
