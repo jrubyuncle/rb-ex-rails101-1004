31984f1ef65925330120486874728824739ebe24
wesleychu <jrubyuncle@gmail.com>
2015-11-03 14:43:00 +0800

    set association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,9 @@
 class Group < ActiveRecord::Base
   has_many :posts
 
+  has_many :group_users
+  has_many :members, through: :group_users, source: :user
+
   belongs_to :owner, foreign_key: :user_id, class_name: 'User'
 
   validates :title, presence: true
