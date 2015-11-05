28de2b2435f5b9127d1fb4da973fc55eddde1589
wesleychu <jrubyuncle@gmail.com>
2015-11-05 12:11:39 +0800

    add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "in seeds"
+
+u = User.create email: "example@gmail.com", name: "example", password: "12345678", password_confirmation: "12345678"
+
+puts u.name
+puts User.count
+
+
+
+20.times do |i|
+	g = Group.create title: "group-aa-#{i}", description:"desc", user_id: u.id
+
+    30.times do |j|
+    	Post.create content: "post-grp#{i}-post#{j}", group_id: g.id, user_id: u.id
+    end
+end
+
+
